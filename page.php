d7099681a17fd841daf493d75d1c628c7526d020
Christian <make.panic@googlemail.com>
2012-02-24 13:12:54 +0100

    fixed viewport meta

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 $template='<html lang="de">
 	<head>
 		<meta charset="utf-8" /> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>%%PAGETITLE%%</title>
 		<link rel="stylesheet" type="text/css" title="" media="all" href="./style.css" />
 	</head>
